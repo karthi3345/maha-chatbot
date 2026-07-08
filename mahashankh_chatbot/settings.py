0aabea53d34ee942a91fde1f7fac757efca38d3c
Karthik_Raja <lkarthikaraja@gmail.com>
2026-07-08 17:43:19 +0530

    chanegs in psql

--- mahashankh_chatbot/settings.py
+++ mahashankh_chatbot/settings.py
@@ -66,10 +66,16 @@ WSGI_APPLICATION = "mahashankh_chatbot.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mahashankh_db",
+        "USER": "myuser",
+        "PASSWORD": "admin@123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
